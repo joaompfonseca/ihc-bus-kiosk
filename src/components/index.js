e0191c0b3493d27f32d1a513f6407219142f0ad5
joaompfonseca <joao.fonseca@ua.pt>
2022-05-28 01:57:11 +0100

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ export {default as LargeActionButton} from './LargeActionButton';
 export {default as Progress} from './Progress';
 export {default as BackButton} from './BackButton';
 export {default as ContinueButton} from './ContinueButton';
-export {default as FunctionButton} from './FunctionButton';
+export {default as AddButton} from './AddButton';
+export {default as EditButton} from './EditButton';
 export {default as BasicDatePicker} from './BasicDatePicker';
