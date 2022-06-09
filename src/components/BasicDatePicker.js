c60ab1ff76bf2316e80706f44d3bf1da281feaff
joaompfonseca <joao.fonseca@ua.pt>
2022-06-09 15:38:13 +0100

    better date picker

--- src/components/BasicDatePicker.js
+++ src/components/BasicDatePicker.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { Grid } from '@mui/material';
 import DatePicker from 'react-datepicker';
+import { LeftButton, RightButton } from '.'
 import 'react-datepicker/dist/react-datepicker.css';
 
 class BasicDatePicker extends Component {
@@ -12,17 +13,26 @@ class BasicDatePicker extends Component {
     render = () => {
         const { action } = this.props;
         const { date } = this.state;
-        
+        const yesterday = new Date(date.getTime() - (1 * 86400000));
+        const today = new Date();
+        const tomorrow = new Date(date.getTime() + (1 * 86400000));
+
         return (
-            <Grid container className='datePicker'>
-                <DatePicker
-                    selected={date}
-                    dateFormat={'dd/MM/yyyy'}
-                    onChange={(date) => { 
-                        this.setState({date: date});
-                        action(date);
-                     }}
-                />
+            <Grid container className='noDivPadding datePicker' padding='0vh'>
+                <Grid item xs={4} align='right'>
+                    <LeftButton action={() => { this.setState({ date: yesterday }); action(yesterday); }} disabled={date.toISOString().split('T')[0] === today.toISOString().split('T')[0]}/>
+                </Grid>
+                <Grid item xs={4}>
+                    <DatePicker
+                        selected={date}
+                        dateFormat={'dd/MM/yyyy'}
+                        minDate={today}
+                        onChange={(date) => { this.setState({ date: date }); action(date); }}
+                    />
+                </Grid>
+                <Grid item xs={4} align='left'>
+                    <RightButton action={() => { this.setState({ date: tomorrow }); action(tomorrow); }} />
+                </Grid>
             </Grid>
         );
     }
